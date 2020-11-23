16daab706e7a6e6eeaab022ca5ca1787af29047e
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2020-11-23 19:13:12 +0530

    binary search recursive

---
+++ Arrayproblems/2)binary-search(recursive).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int binary(int arr[],int s,int e,int x){
+    while(e >= s){
+        int mid = s+(e-s)/2;
+        if(arr[mid] == x) return mid;
+        if(arr[mid] < x) return binary(arr,mid+1,e,x);
+        if(arr[mid] > x) return binary(arr,s,mid-1,x);
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {10,20,30,40,50,60};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int x = 30;
+    int result = binary(arr,0,size-1,x);
+    (result != -1) ? cout<<"found : "<<result
+                   : cout<<"not found"; 
+
+    return 0;
+}
