11dadf5f7eb3031ac8438036898e6640829a54e7
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2020-12-29 21:26:34 +0530

    reverse a linkedlist in group of size

---
+++ Linkedlist/Reverse-a-Linkedlist-in-group-of-given-Size.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node(int data){
+            this->data = data;
+            next = NULL;
+        }
+};
+
+Node *takeInput(){
+    int data;
+    cout<<"Enter the list -> ";
+    cin>>data;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1){
+        Node *n = new Node(data);
+        if(head == NULL){
+            head = n;
+            tail = n;
+        }else{
+            tail->next = n;
+            tail = n;
+        }
+        cin>>data;
+    }
+    return head;
+}
+
+void PrintLL(Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"-> ";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}
+
+Node *reverseLL(Node *head, int n){
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *temp = NULL;
+    Node *tail = NULL;
+    Node *newHead = NULL;
+    Node *join = NULL;
+    int group = 0;
+    while(curr){
+        join = curr;
+        group = n;
+        prev = NULL;
+
+        while(curr && group--){
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        if(!newHead){
+            newHead = prev;
+        }
+        if(tail){
+            tail->next = prev;
+        }
+        tail = join;
+    }
+    return newHead;
+}
+
+int main(){
+    Node *head = takeInput();
+    cout<<"The initial list is : ";
+    PrintLL(head);
+    cout<<endl;
+    cout<<"enter number of group to reverse -> ";
+    int n;
+    cin>>n;
+    head = reverseLL(head,n);
+    cout<<"The final list is : ";
+    PrintLL(head);
+
+    return 0;
+}
