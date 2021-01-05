92585d0b367e65fb2b7d2b5bb316f15cc103d687
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2021-01-06 01:14:14 +0530

    reverse a string

---
+++ String/Reverse-a-String.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void reverseString(string s){
+    for(int i=s.length()-1; i>=0; i--){
+        cout<<s[i];
+    }
+}
+
+int main(){
+    string s = "Abhisek Ray";
+    reverseString(s);
+
+    return 0;
+}
