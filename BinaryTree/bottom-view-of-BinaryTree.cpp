a1626abbfa515e8991afe0e11e2d29d54f407a1f
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2021-01-21 17:06:09 +0530

    bottom view of binary tree

---
+++ BinaryTree/bottom-view-of-BinaryTree.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<map>
+using namespace std;
+
+template<typename T>
+class BTNode{
+    public:
+        int data;
+        BTNode *left;
+        BTNode *right;
+        BTNode(T data){
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+void bottomviewBT(BTNode<int> *root){
+    map<int,int> m;
+    queue<pair<BTNode<int>*,int> > q;
+    if(!root) return;
+    q.push({root,0});
+    while(!q.empty()){
+        BTNode<int> *f = q.front().first;
+        int height = q.front().second;
+        q.pop();
+        m[height] = f->data;
+        if(f->left) q.push({f->left, height-1});
+        if(f->right) q.push({f->right, height+1});
+    }
+    for(auto x:m) cout<<x.second<<" ";
+}
+
+int main(){
+    BTNode<int> *root = new BTNode<int>(1);
+    root->left = new BTNode<int>(2);
+    root->right = new BTNode<int>(3);
+    root->left->left = new BTNode<int>(4);
+    root->left->right = new BTNode<int>(5);
+    root->right->left = new BTNode<int>(6);
+    root->right->right = new BTNode<int>(7);
+
+    cout<<"The bottom view of the Binary Tree is : ";
+    bottomviewBT(root);
+
+    return 0;
+}
