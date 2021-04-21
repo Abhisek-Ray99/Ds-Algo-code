b80da6542a3e10d25d738990b9a08dd65f42830a
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2021-04-21 11:03:21 +0530

    Update Reverse-a-String.cpp

--- String/Reverse-a-String.cpp
+++ String/Reverse-a-String.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 using namespace std;
 
-void reverseString(string s){
-    for(int i=s.length()-1; i>=0; i--){
-        cout<<s[i];
+void reverse_Str(string &S,int s,int e){
+   while(s < e){
+       swap(S[s],S[e]);
+       s++;
+       e--;
+   }
+}
+
+void Print_Str(string S){
+    for(int start=0; start<S.length(); start++){
+        cout<<S[start]<<" ";
     }
 }
 
 int main(){
-    string s = "Abhisek Ray";
-    reverseString(s);
+    string S = "Abhisek";
+    cout<<"Before reversing the array : ";
+    Print_Str(S);
+    reverse_Str(S,0,S.length()-1);
+    cout<<"\nAfter reversing the array : ";
+    Print_Str(S);
 
     return 0;
 }
