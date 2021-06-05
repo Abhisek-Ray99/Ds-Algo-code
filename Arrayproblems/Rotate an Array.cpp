c8ea25ce90d532a13f910f159f00d7eb4651f4e7
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2021-06-05 10:31:08 +0530

    Update Rotate an Array.cpp

--- Arrayproblems/Rotate an Array.cpp
+++ Arrayproblems/Rotate an Array.cpp
@@ -1,3 +1,7 @@
+//Brute force approch
+
+
+//rotate an array from left to right
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -19,6 +23,35 @@ int main(){
     arr = rotateArr(arr,ele);
     for(int s=0; s<arr.size(); s++){
         cout<<arr[s]<<" ";
+    }
+
+    return 0;
+}
+
+
+
+//rotate an array from right to left
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> rotatenums(vector<int> nums,int k){
+    while(k--){
+        int temp = nums[nums.size()-1];
+        for(int i=nums.size()-2; i >= 0; i--){
+            nums[i+1] = nums[i];
+        }
+        nums[0] = temp;
+    }
+    return nums;
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    nums = rotatenums(nums,k);
+    for(int s=0; s<nums.size(); s++){
+        cout<<nums[s]<<" ";
     }
 
     return 0;
