c34ca0441dd22272d0ac247ed0583a85e877cdc1
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2022-02-23 23:07:02 +0530

    Create print Inorder successor in BST (brute force).cpp

---
+++ Binary-search-tree/print Inorder successor in BST (brute force).cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+    public:
+        int data;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+class BST{
+    TreeNode *root;
+    vector<int> inorder;
+    TreeNode *insert(TreeNode *root, int node){
+        if(root == NULL){
+            TreeNode *newnode = new TreeNode(node);
+            return newnode;
+        }
+        if(node < root->data){
+            root->left = insert(root->left,node);
+        }else{
+            root->right = insert(root->right,node);
+        }
+        return root;
+    }
+    void printBST(TreeNode *root){
+        if(root == NULL) return;
+        cout<<root->data<<": ";
+        if(root->left) cout<<"L->"<<root->left->data<<" ";
+        if(root->right) cout<<"R->"<<root->right->data;
+        cout<<endl;
+        printBST(root->left);
+        printBST(root->right);
+    }
+    void getInorder(TreeNode *root){
+        if(root == NULL) return;
+        getInorder(root->left);
+        inorder.push_back(root->data);
+        getInorder(root->right);
+    }
+    public:
+        BST(){
+            root = NULL;
+        }
+        void insert(int node){
+            root = insert(root,node);
+        }
+        void printBST(){
+            printBST(root);
+        }
+        void InorderSuccessor(int node){
+            getInorder(root);
+            for(int start=0; start<inorder.size(); start++){
+                if(inorder[start] == node && inorder[inorder.size()-1] != node){
+                    cout<<inorder[start+1];
+                    return;
+                }else if(inorder[start] == node){
+                    cout<<"NULL";
+                    return;
+                }
+            }
+        }
+};
+
+int main(){
+    BST b;
+    b.insert(12);
+    b.insert(6);
+    b.insert(20);
+    b.insert(3);
+    b.insert(8);
+    b.insert(13);
+    b.insert(24);
+    b.printBST();
+    b.InorderSuccessor(24);
+
+    return 0;
+}
