030dd1f88b8b908362f132e2fbb9a88f612b55ff
Abhisek Ray <66687450+Abhisek-Ray99@users.noreply.github.com>
2022-02-24 20:31:36 +0530

    Update Populate-InOrder-Successor-of-all-Nodes.cpp

--- Binary-search-tree/Populate-InOrder-Successor-of-all-Nodes.cpp
+++ Binary-search-tree/Populate-InOrder-Successor-of-all-Nodes.cpp
@@ -1,4 +1,101 @@
 // populate InOrder successor of all nodes
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+    public:
+        int data;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode *next;
+        TreeNode(int data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+            this->next = NULL;
+        }
+};
+
+class BST{
+    TreeNode *root;
+    TreeNode *insert(TreeNode *root,int node){
+        if(root == NULL){
+            TreeNode *newnode = new TreeNode(node);
+            return newnode;
+        }
+        if(node < root->data){
+            root->left = insert(root->left,node);
+        }else{
+            root->right = insert(root->right,node);
+        }
+        return root;
+    }
+    void printBST(TreeNode *root){
+        if(root == NULL) return;
+        cout<<root->data<<": ";
+        if(root->left) cout<<"L->"<<root->left->data<<", ";
+        if(root->right) cout<<"R->"<<root->right->data;
+        cout<<endl;
+        printBST(root->left);
+        printBST(root->right);
+    }
+    void populateIn_successor(TreeNode *root,TreeNode *&prev){
+        if(root == NULL) return;
+        populateIn_successor(root->left,prev);
+        if(prev != NULL){
+            prev->next = root;
+        }
+        prev = root;
+        populateIn_successor(root->right,prev);
+    }
+    public:
+        BST(){
+            root = NULL;
+        }
+        void insert(int node){
+            root = insert(root,node);
+        }
+        void printBST(){
+            printBST(root);
+        }
+        void populateIn_successor(){
+            TreeNode *prev = NULL;
+            populateIn_successor(root,prev);
+            TreeNode *start = root;
+            while(start->left != NULL){
+                start = start->left;
+            }
+            while(start != NULL){
+                if(start->next == NULL){
+                    cout<<prev->data<<"->"<<"NULL";
+                }else{
+                    cout<<start->data<<"->"<<start->next->data<<" ";
+                }
+                start = start->next;
+            }
+        }
+};
+
+int main(){
+    BST b;
+    b.insert(12);
+    b.insert(6);
+    b.insert(28);
+    b.insert(4);
+    b.insert(10);
+    b.insert(30);
+    b.insert(22);
+    b.insert(9);
+    b.printBST();
+    b.populateIn_successor();
+
+    return 0;
+}
+
+
+
+// -----------------------------------------------------------------------------------Another method--------------------------------------------------------------------------
+
 
 #include<iostream>
 using namespace std;
